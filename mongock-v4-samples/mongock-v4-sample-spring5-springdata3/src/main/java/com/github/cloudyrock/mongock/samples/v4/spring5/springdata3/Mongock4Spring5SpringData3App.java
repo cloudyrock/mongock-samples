b3e7e4460d2374648f5f57aab7b2034ba79e03c2
dieppa <aperezdieppa@gmail.com>
2020-05-21 07:46:04 +0100

    gitignore

---
+++ mongock-v4-samples/mongock-v4-sample-spring5-springdata3/src/main/java/com/github/cloudyrock/mongock/samples/v4/spring5/springdata3/Mongock4Spring5SpringData3App.java
@@ -0,0 +1,93 @@
+package com.github.cloudyrock.mongock.samples.v4.spring5.springdata3;
+
+import com.github.cloudyrock.mongock.driver.mongodb.springdata.v3.SpringDataMongo3Driver;
+import com.github.cloudyrock.mongock.samples.v4.spring5.springdata3.changelogs.ClientChangeLog;
+import com.github.cloudyrock.mongock.samples.v4.spring5.springdata3.client.ClientRepository;
+import com.github.cloudyrock.mongock.samples.v4.spring5.springdata3.spring.DateToZonedDateTimeConverter;
+import com.github.cloudyrock.mongock.samples.v4.spring5.springdata3.spring.ZonedDateTimeToDateConverter;
+import com.github.cloudyrock.spring.MongockSpring5;
+
+import com.mongodb.client.ClientSession;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.mongodb.SessionSynchronization;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@SpringBootApplication
+@EnableMongoRepositories(basePackageClasses = ClientRepository.class)
+public class Mongock4Spring5SpringData3App {
+
+  public final static String CLIENTS_COLLECTION_NAME = "clientCollection";
+
+  public static void main(String[] args) {
+    SpringApplication.run(Mongock4Spring5SpringData3App.class, args);
+  }
+
+  @Bean
+  @Profile("!initializingBean")
+  public MongockSpring5.MongockApplicationRunner mongockApplicationRunner(ApplicationContext springContext,
+                                                                          MongoTemplate mongoTemplate,
+                                                                          @Value("${mongock.lock.lockAcquiredForMinutes:5}") long lockAcquiredForMinutes,
+                                                                          @Value("${mongock.lock.maxWaitingForLockMinutes:3}") long maxWaitingForLockMinutes,
+                                                                          @Value("${mongock.lock.maxTries:3}") int maxTries) {
+
+    SpringDataMongo3Driver driver = new SpringDataMongo3Driver(mongoTemplate);
+    driver.setChangeLogCollectionName("new_changelog_collection_name");//optional
+    driver.setLockCollectionName("new_lock_collection_name");//optional
+    return MongockSpring5.builder()
+        .setDriver(driver)
+        .addChangeLogsScanPackage(ClientChangeLog.class.getPackage().getName())
+        .setLockConfig(lockAcquiredForMinutes, maxWaitingForLockMinutes, maxTries)//optional
+        .setSpringContext(springContext)
+        .buildApplicationRunner();
+  }
+
+
+
+  @Bean
+  @Profile("initializingBean")
+  public MongockSpring5.MongockInitializingBeanRunner mongockInitializingBeanRunner(ApplicationContext springContext,
+                                                                                    MongoTemplate mongoTemplate,
+                                                                                    @Value("${mongock.lock.lockAcquiredForMinutes:5}") long lockAcquiredForMinutes,
+                                                                                    @Value("${mongock.lock.maxWaitingForLockMinutes:3}") long maxWaitingForLockMinutes,
+                                                                                    @Value("${mongock.lock.maxTries:3}") int maxTries) {
+    SpringDataMongo3Driver driver = new SpringDataMongo3Driver(mongoTemplate);
+    driver.setChangeLogCollectionName("new_changelog_collection_name");//optional
+    driver.setLockCollectionName("new_lock_collection_name");//optional
+    return MongockSpring5.builder()
+        .setDriver(driver)
+        .addChangeLogsScanPackage(ClientChangeLog.class.getPackage().getName())
+        .setLockConfig(lockAcquiredForMinutes, maxWaitingForLockMinutes, maxTries)//optional
+        .setSpringContext(springContext)
+        .buildInitializingBeanRunner();
+  }
+
+
+  @Bean
+  public MongoCustomConversions customConversions() {
+    List<Converter<?, ?>> converters = new ArrayList<>();
+    converters.add(DateToZonedDateTimeConverter.INSTANCE);
+    converters.add(ZonedDateTimeToDateConverter.INSTANCE);
+    return new MongoCustomConversions(converters);
+  }
+
+}
